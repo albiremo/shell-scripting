457271ca6887622ec7d9ca289531dafdcf48c339
albiremo <albertoremigi92@gmail.com>
2017-10-04 17:50:45 +0200

    Adding velocity step statistics

--- resubmit.sh
+++ resubmit.sh
@@ -17,38 +17,32 @@ echo "ulimit -s unlimited"                                      >> $1.batch
 echo "ulimit -t unlimited"                                      >> $1.batch
 
 echo "#run_applicarion"                                         >> $1.batch
-echo "sleep 5"                                                  >> $1.batch
+echo "cat Runtimedata >> Run"                                   >> $1.batch
+# adds a line to theend every iteration
+echo "((RSAVE_STEP++))"                                       >> $1.batch
+echo "echo \$RSAVE_STEP >> theend$1.txt"                   >> $1.batch
+#count lines
+echo "d=\$(wc -l < theend$1.txt)"                        >>$1.batch
+echo "((d++))"                                             >>$1.batch
+echo "mkdir \$d"                                         >>$1.batch
+echo "mv stats* \$d/"                                        >>$1.batch
 
 
-#if file primo is in the directory
-#initialize counter RSAVE_STEP
-# and delete primo
-#else write the step on theend file
 
-echo "if [ -n "primo" ]"                                       >> $1.batch
-echo "then"                                                    >> $1.batch
-echo " RSAVE_STEP=1"                                           >> $1.batch
-echo "rm primo"                                                >> $1.batch
-echo "else"                                                    >> $1.batch
-echo " RSAVE_STEP=RSAVE_STEP+1"                                >> $1.batch
-echo " echo \$RSAVE_STEP >> theend$1.txt" >> $1.batch
-echo "fi"                                                      >> $1.batch
+
 
 # Check if the job is finished and if it is not
 # resubmit this file
 #
 echo "if grep CONTINUE ../restart.txt"                       >> $1.batch
 echo "then"                                                  >> $1.batch
-echo "  qsub $1.batch"                                       >> $1.batch
+echo "sleep 3"                                              >>$1.batch
+echo "qsub $1.batch"                                        >> $1.batch
 echo "elif grep FINISHED ../restart.txt"                            >> $1.batch
 echo "then"                                                  >> $1.batch
 echo "echo dopo \$RSAVE_STEP iterazioni concluso il lavoro $1 >> theend$1.txt" >> $1.batch
 echo "fi" >> $1.batch
 echo "exit 0" >> $1.batch
-    
+
 qsub $1.batch
 sleep 3
-qstat -u aremigi0
-
-
-
