aa4a09f672cb754c73d36200096d377a585fec0e
albiremo <albertoremigi92@gmail.com>
2017-10-31 12:18:25 +0100

    fixed reduce

--- reduce.sh
+++ reduce.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+rm Run_reduced
 awk '{print $1,$2,$3,$4,$5,$6}' Run > Run_b
 uniq Run_b > Run_clean
 rm Run_b
-sed "1,$1'd'" Run_clean > Run_reduced
+sed "1,5000d" Run_clean > Run_reduced
 rm Run_clean
-k = $(wc -l < Run_reduced)
-echo "$k" >> situazione.txt
-./meanandvar < Run_clean 3 >> situazione.txt
+LINES=$(wc -l < Run_reduced)
+echo $LINES
+echo "$LINES" >> situazione.txt
+./meanandvar < Run_reduced 3 >> situazione.txt
 
 
